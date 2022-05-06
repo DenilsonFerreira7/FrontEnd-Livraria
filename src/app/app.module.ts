4c028ab7e17ee7b6ffe2d727be5c00d84d600bd6
Denilson Ferreira <denilsonfsantos7@gmail.com>
2022-05-06 11:04:26 -0300

    nova att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
 import { FormsModule } from "@angular/forms";
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 
 
@@ -48,7 +50,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatButtonModule,
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
